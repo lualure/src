3b0c15b36c3a8e0d857b5faef667b068c99f5599
Tim Menzies <tim.menzies@gmail.com>
2017-07-15 01:34:26 -0400

    saving

--- lib/sdtree.lua
+++ lib/sdtree.lua
@@ -5,35 +5,37 @@
 require "show"
 local the=require "config"
 local num=require "num"
-local lst=require "lst"
+local lst=require "lists"
+local row=require "row"
 ------------------------------------------------
 local function create(t0,rows)  return {
   here=tbl.copy(t0,rows),
   breaks={}}
 end
 ------------------------------------------------
-local function rankcolumns(t,head, y,z,     cols) 
-  y = y or function (_) row.dominate(_,t) end
-  z = z or function (_) return z.sd end
+return function (t,y,z,     cols) 
+  y = y or function (_) return row.dominate(_,t) end
+  z = z or function (_) return _.sd end
   -----------------------------------------------
   local function xpect(col)
     if not col._score then
       col._score=0
       for _,num in pairs(col.nums) do
-        col._score = col.w + z(num)* num.n / col.n end end
+        col._score = col._score+ z(num)* num.n / col.n end end
     return col._score end
   -----------------------------------------------
-  local function gather(head)
-    local col = {_score = nil, pos=head.pos, nums={},n=0} 
-    for _,row in t.rows do
+  local function whatif(head)
+    local col = {pos=head.pos, nums={},_score=nil,n=0} 
+    for _,row in pairs(t.rows) do
        local x = row.cells[col.pos]
+       print(row, y(row))
        if x ~= the.ignore then
          col.n = col.n + 1
          col.nums[x] = num.update(col.nums[x] or num.create(), 
                                   y(row))  end end
     return col end
   -------------------  
-  return lst.sort( collect(t.x.cols, gather),
+  return lst.sort( lst.collect(t.x.cols, whatif),
                    function(a,b) 
                      return xpect(a) < xpect(b) end)
 end
