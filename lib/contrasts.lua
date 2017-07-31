aa4d65f8c1982b843089b26640d96e5904c74080
Tim Menzies <tim.menzies@gmail.com>
2017-07-31 18:44:45 -0400

    saving

--- lib/contrasts.lua
+++ lib/contrasts.lua
@@ -13,22 +13,43 @@ local function branches(tr,out,b)
       branches(kid,out,LST.copy(b)) end  
   return out end
 
-local function plans(branches)
+local function monitors(branches)
+  return plans(branches, function(x,y)
+               return x < y end) end
+
+local function hold(t,attr,val)
+   local old = t[attr] or {}
+   if old[val] 
+     then old[val] = old[val] + 1 
+     else old[val] = 1 end
+   return t end
+
+local function holds(branch)
+  local out = {}
+  for _,step in pairs(branch) do
+    --print(step)
+    out = hold(out,step[1],step[2]) end
+  return out end
+
+local function plans(branches, better)
+  better = better or function(x,y) return x > y end
   for i,branch1 in pairs(branches) do
     for j,branch2 in pairs(branches) do
       if i ~= j then
         local num1=LST.last(branch1)[3]
         local num2=LST.last(branch2)[3]
-        if num2.mu > num1.mu then
+        if better(num2.mu, num1.mu)  then
           if not NUM.same(num1,num2) then
-            print(i,j,(num2.mu - num1.mu)/num1.mu) end end end end end end
+            print(i,j,(num2.mu - num1.mu)/num1.mu, holds(branch1),holds(branch2)) end end end end end end
+
+
 
 defaults()
 the.tree.min=4
 x=TREES.xomo()
 b=branches(x,{},{})
-LST.maprint(b)
+--LST.maprint(b)
 
-TREE.show(x)
+--TREE.show(x)
 
 plans(b)
