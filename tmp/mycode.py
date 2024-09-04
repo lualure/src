77bc145bcd029a8b6cfce6d41020185e85b27ee0
Tim Menzies <menzies.tim@gmail.com>
2024-09-04 18:52:11 +0000

    saving

--- tmp/mycode.py
+++ tmp/mycode.py
@@ -1,24 +1,40 @@
-import random,sys,ast
+#/usr/bin/env python3.13
+# 
+# import random,sys,ast
 
-def coerce(s):
+atom = str | bool | int | float
+
+def coerce(s:str) -> atom:
+    "strig"
     try: return ast.literal_eval(s)
     except Exception:return s
 
-def arg(i): return coerce(sys.argv[i] if i <= len(sys.argv)-1 else "")
-def noop(*l): return l
+def arg(i:int) -> atom: 
+    "parse item 'i' from the command line"
+    return coerce(sys.argv[i] if i <= len(sys.argv)-1 else "")
+
+def noop(*l: list) -> list: 
+    return l
 
 #For mode colors, exec
 # https://gist.github.com/rene-d/9e584a7dd2935d0f461904b9f2950007
-def red(s):    return f"\033[0;31m{s}\033[0m"
-def green(s):  return f"\033[0;32m{s}\033[0m"
-def yellow(s): return f"\033[1;33m{s}\033[0m"
+def red(s:str)    -> str:  return f"\033[0;31m{s}\033[0m"
+def green(s:str)  -> str:  return f"\033[0;32m{s}\033[0m"
+def yellow(s:str) -> str:  return f"\033[1;33m{s}\033[0m"
 
 class go:
-    def _all():   return [s for s in dir(go) if  s[0] !="_"]
-    def _show(s): 
+    def _all(): 
+        "return all public actions"  
+        return [s for s in dir(go) if  s[0] !="_"]
+    
+    def _show(s:str): 
+        "descrine action 's'"
         doc = getattr(go,s).__doc__ or ""
         print(f"\t-{yellow(s)}\t: {doc.replace("\n"," ")}")
-    def show(_):  [go._show(s) for s in go._all()]
+
+    def show(_): 
+        "describe all actions"
+        [go._show(s) for s in go._all()]
     
     def noop(_): 
         "do nothing"
@@ -44,5 +60,4 @@ for i,s in enumerate(sys.argv):
     fun = getattr(go, s, None)
     if fun:
         random.seed(SEED)
-        
         fun( arg(i+1) ) 
